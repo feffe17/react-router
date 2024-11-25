06803f2dae14604d8f1ecf4f935b349d8a670751
feffe1702 <fpm.asd@gmail.com>
2024-11-25 15:54:17 +0100

    [WIP] create new pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import './App.css'
 import AppHeader from './assets/AppHeader.jsx'
 import AppMain from './assets/AppMain'
 import AppFooter from './assets/AppFooter'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import AboutUs from "./pages/AboutUs.jsx"
+import Blog from './pages/HomePage.jsx'
+import DefaultLayout from "./pages/DefaultLayout.jsx"
 
 function App() {
 
   return (
     <>
-      <AppHeader />
-      <AppMain />
-      <AppFooter />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />} >
+            <Route index element={<HomePage />} />
+            <Route path='/about' element={<AboutUs />} />
+            <Route path='/blog' element={<Blog />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "../assets/AppHeader";
+import AppMain from "../assets/AppMain";
+import AppFooter from "../assets/AppFooter";
+
+export default function DefaultLayout() {
+    return (
+        <>
+            <AppHeader />
+            <AppMain />
+            <AppFooter />
+        </>
+
+    )
+}
